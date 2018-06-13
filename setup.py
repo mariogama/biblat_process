61825aa174b38f5777ddbf5269876f6777f951bd
Arturo Rendón Cruz <arendon@dgb.unam.mx>
2018-06-13 12:36:11 -0500

    Creación de paquete biblat_process con setup.py y requerimientos

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name="biblatprocess",
+    version='0.0',
+    description="Proceso de archivos en formato ALEPH secuencial a mongoDB",
+    author="DGB Sistemas",
+    author_email="sistemasintegralesdgb@dgb.unam.mx",
+    license="GPL-3.0",
+    url="https://github.com/dgb-sistemas/biblat_process",
+    packages=find_packages(),
+    keywords='biblat process aleph marc mongo',
+    maintainer_email='sistemasintegralesdgb@dgb.unam.mx',
+    download_url='',
+    classifiers=[],
+    install_requires=[],
+    tests_require=[],
+    dependency_links=[],
+    test_suite=''
+)
